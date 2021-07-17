875a60a448f4461ec255f6846c08cdd067939171
zypose <1668493630@qq.com>
2021-07-17 15:23:38 +0800

    add explan of main function

--- src/main/java/com/example/house/HouseApplication.java
+++ src/main/java/com/example/house/HouseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HouseApplication {
-
+    //the begining of application
     public static void main(String[] args) {
         SpringApplication.run(HouseApplication.class, args);
     }
